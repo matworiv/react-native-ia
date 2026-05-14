0046f2edf213b8521023f6e354f0a4714ac54565
Aluno TI <alunoti@univaglabs.iemat>
2026-05-14 10:18:37 -0400

    inicialização da estrutura base

---
+++ app/sucesso.tsx
@@ -0,0 +1,13 @@
+
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+export default function SucessoScreen(){
+    return (
+        <SafeAreaView>
+            <View>
+                <Text> Sucesso Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
